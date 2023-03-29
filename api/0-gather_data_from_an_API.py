a6ea5ce4175347ffddb2e4355fe98b262ad1c683
juanivalle <juanignaciovalle18@gmail.com>
2023-03-29 12:38:32 -0300

    task 0

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -9,7 +9,8 @@ if __name__ == '__main__':
     employee_id = int(sys.argv[1])
 
     url = "https://jsonplaceholder.typicode.com/todos?userId={employee_id}"
-    user_url = "https://jsonplaceholder.typicode.com/users/{}".format(employee_id)
+    user_url = "https://jsonplaceholder.typicode.com/users/{}".format(
+        employee_id)
 
     done_task = 0
     total_tasks = 0
@@ -21,7 +22,8 @@ if __name__ == '__main__':
         total_tasks += 1
         if task['completed'] is True:
             done_task += 1
-    print("Employee {} is done with {}/{}:".format(usr_response, done_task, total_tasks))
+    print("Employee {} is done with {}/{}:".format(usr_response,
+                                                    done_task, total_tasks))
 
     for task in response:
         if task['completed'] is True:
