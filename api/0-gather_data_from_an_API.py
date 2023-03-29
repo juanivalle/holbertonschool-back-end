3cfa1fa1f6f007e3a014b93a9849459c119120f5
juanivalle <juanignaciovalle18@gmail.com>
2023-03-29 13:10:27 -0300

    task 0

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -10,11 +10,11 @@ if __name__ == '__main__':
     users_url = "https://jsonplaceholder.typicode.com/users"
     todos_url = "https://jsonplaceholder.typicode.com/todos"
 
-    done_tasks = requests.get(todos_url, params= {
+    done_tasks = requests.get(todos_url, params={
         'completed': 'True', 'user_Id': USER_ID}).json
-    not_done_tasks = requests.get(todos_url, params= {
+    not_done_tasks = requests.get(todos_url, params={
         'completed': 'False', 'user_Id': USER_ID}).json
-    info = requests.get(users_url, params= {'id': USER_ID}).json()
+    info = requests.get(users_url, params={'id': USER_ID}).json()
 
     EMPLOYEE_NAME = info[0]['name']
     num_done_tasks = len(done_tasks)
